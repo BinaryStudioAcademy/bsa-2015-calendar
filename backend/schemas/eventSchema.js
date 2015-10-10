5b9eef1a6cdeaff961fa5cfc956a5576c198f0c0
tomatoua <tomato.ua@gmail.com>
2015-10-10 21:29:28 +0300

    add event schema

--- backend/schemas/eventSchema.js
+++ backend/schemas/eventSchema.js
@@ -2,7 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var eventSchema = new Schema({
-	name: String
+    ownerId : {type Schema.Types.ObjectId, ref: 'User'},
+    title : String,
+    description : String,
+    plan : {type Schema.Types.ObjectId, ref: 'Plan'},
+    isPrivate : Boolean,
+    start: Date, 
+    end: Date, 
+    users: [{type Schema.Types.ObjectId, ref: 'User'}]
+    room: {type Schema.Types.ObjectId, ref: 'Room'},
+    devices: [{type Schema.Types.ObjectId, ref: 'Device'}],
 });
 
 module.exports = mongoose.model('Event', eventSchema);
