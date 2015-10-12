74149c2818c268586cb22157941167e52dfcc3bb
SupraVlad <stepler94mg@gmail.com>
2015-10-13 01:09:52 +0300

    Half-ready version of the event tab

---
+++ frontend/javascripts/eventsCalendar/eventsCalendarController.js
@@ -0,0 +1,10 @@
+var app = require('../app');
+
+app.controller('EventsViewController', EventsViewController);
+
+
+function EventsViewController() {
+	var vm = this;
+	// vm.events = 
+	// some methods that will filter the content
+}
