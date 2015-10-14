42f1f5e1e66c4e56a9d733758457bdc64bf92235
SupraVlad <stepler94mg@gmail.com>
2015-10-14 09:26:41 +0300

    Created functionality for the calendar events.

--- frontend/javascripts/eventsCalendar/eventsCalendarController.js
+++ frontend/javascripts/eventsCalendar/eventsCalendarController.js
@@ -2,9 +2,23 @@ var app = require('../app');
 
 app.controller('EventsViewController', EventsViewController);
 
-
 function EventsViewController() {
 	var vm = this;
-	// vm.events = 
-	// some methods that will filter the content
+	vm.events = [
+	{
+		date: 'April 9, 1999',
+		duration: 'Whole day',
+		description: "Someone's b-day."
+	},
+	{
+		date: 'May 13, 2014',
+		duration: '9.00-12.00',
+		description: 'Meeting with foreign customers.'
+	},
+	{
+		date: 'February 27, 2001',
+		duration: '14.00-17.00',
+		description: 'Long daily meeting'
+	}];
+	
 }
