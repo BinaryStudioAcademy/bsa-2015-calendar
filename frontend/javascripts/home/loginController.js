4867dbb63982c84ceb1b787a488605aef1721067
Vitalii Kalchuk <v.sweak@gmail.com>
2015-10-14 12:44:51 +0300

    Playing with forms

--- frontend/javascripts/home/loginController.js
+++ frontend/javascripts/home/loginController.js
@@ -5,7 +5,15 @@ var app = require('../app');
 
 app.controller('LoginController', function ($scope) {
     $scope.isVisible = true;
+
     $scope.showHide = function () {
         $scope.isVisible = $scope.isVisible ? false : true;
+    };
+
+    $scope.login = function () {
+        console.log($scope.inputEmail);
+        console.log($scope.inputPassword);
+        $scope.inputEmail = '';
+        $scope.inputPassword = '';
     };
 });
