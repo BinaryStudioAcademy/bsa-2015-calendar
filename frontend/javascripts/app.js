cb5ff2622aa825c22bd581f3dc42a6a5a6b7e325
Vitalii Kalchuk <v.sweak@gmail.com>
2015-10-20 20:45:24 +0300

    Code formatted

--- frontend/javascripts/app.js
+++ frontend/javascripts/app.js
@@ -1,25 +1,25 @@
 var app = angular.module('calendar-app', ['ui.router', 'ngResource', 'ui.bootstrap', 'ngAnimate'])
-	.config(['$stateProvider', '$urlRouterProvider','$resourceProvider', '$httpProvider', '$locationProvider', 
-		function($stateProvider, $urlRouterProvider, $resourceProvider, $httpProvider, $locationProvider) {
-			$urlRouterProvider.otherwise('/');
-			$stateProvider
-				.state('home',{
-					url: '',
-					templateUrl: './templates/layout/layout.html',
-					controller: 'LayoutController',
-					controllerAs: 'LayoutCtrl',
-					redirectTo: 'home.start'
-				})
-				.state('home.start', {
-					url: '/',
-					templateUrl: './templates/home/homepage.html'
-				})
-				.state('calendar', {
-					url: '/calendar',
-					templateUrl: './templates/calendar/calendar.html',
-					controller: 'CalendarController',
-					controllerAs: 'calendarCtrl'
-				})
+    .config(['$stateProvider', '$urlRouterProvider', '$resourceProvider', '$httpProvider', '$locationProvider',
+        function ($stateProvider, $urlRouterProvider, $resourceProvider, $httpProvider, $locationProvider) {
+            $urlRouterProvider.otherwise('/');
+            $stateProvider
+                .state('home', {
+                    url: '',
+                    templateUrl: './templates/layout/layout.html',
+                    controller: 'LayoutController',
+                    controllerAs: 'LayoutCtrl',
+                    redirectTo: 'home.start'
+                })
+                .state('home.start', {
+                    url: '/',
+                    templateUrl: './templates/home/homepage.html'
+                })
+                .state('calendar', {
+                    url: '/calendar',
+                    templateUrl: './templates/calendar/calendar.html',
+                    controller: 'CalendarController',
+                    controllerAs: 'calendarCtrl'
+                })
                 .state('signIn', {
                     url: '/signIn',
                     templateUrl: './templates/home/signIn.html',
@@ -30,19 +30,19 @@ var app = angular.module('calendar-app', ['ui.router', 'ngResource', 'ui.bootstr
                     templateUrl: './templates/home/signUp.html',
                     controller: 'LoginController'
                 })
-				.state('calendar.dayView', {
-					url: '/calendar/dayView',
-					templateUrl: './templates/dailyCalendar/dailyCalendarTemplate.html',
-					controller: 'DayViewController',
-					controllerAs: 'dvCtrl'
-				})
-				.state('calendar.monthView', {
-					url: '/calendar/monthView',
-					templateUrl: './templates/monthCalendar/monthCalendar.html',
-					controller: ''
-				});
-		}
-	]);
+                .state('calendar.dayView', {
+                    url: '/calendar/dayView',
+                    templateUrl: './templates/dailyCalendar/dailyCalendarTemplate.html',
+                    controller: 'DayViewController',
+                    controllerAs: 'dvCtrl'
+                })
+                .state('calendar.monthView', {
+                    url: '/calendar/monthView',
+                    templateUrl: './templates/monthCalendar/monthCalendar.html',
+                    controller: ''
+                });
+        }
+    ]);
 
 app.run(['$rootScope', '$state', function ($rootScope, $state) {
     $rootScope.$on('$stateChangeStart', function (evt, to, params) {
