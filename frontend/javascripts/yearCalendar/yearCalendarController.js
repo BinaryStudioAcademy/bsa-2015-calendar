3dcd58a50fac686fd02d617e8ce1cb3443cd1827
ag <andrey.grisch@gmail.com>
2015-11-02 10:36:01 +0200

    add new event modal yearView

--- frontend/javascripts/yearCalendar/yearCalendarController.js
+++ frontend/javascripts/yearCalendar/yearCalendarController.js
@@ -2,9 +2,9 @@ var app = require('../app');
 
 app.controller('yearCalendarController', yearCalendarController);
 
-yearCalendarController.$inject = ['calendarService', '$scope'];
+yearCalendarController.$inject = ['calendarService', 'yearEventService', '$scope', '$uibModal', '$timeout'];
 
-function yearCalendarController(calendarService, $scope) {
+function yearCalendarController(calendarService, yearEventService, $scope, $uibModal, $timeout) {
     var vm = this;
 
     //init with current year
@@ -38,6 +38,136 @@ function yearCalendarController(calendarService, $scope) {
         }, function(error) {
             console.log(error);
         });
+    }
+
+
+    $scope.showDay = function(step) {
+        var date = new Date($scope.selectedDate);
+
+        date.setDate(
+            step === 1 ?
+                date.getDate() + 1
+                    :
+                date.getDate() - 1
+        );
+
+        $scope.selectedDate = date;
+    };
+
+    $scope.showDate = function() {
+        console.log($scope.selectedDate);
+    };
+
+    $scope.toggleModal = function() {
+        $scope.modalShown = !$scope.modalShown;
+    };
+
+    $scope.showCloseModal = function(month, dayDate) {
+        console.log(month, dayDate);
+        $scope.selectedDate = new Date(vm.currentYear, +month, dayDate);
+        $scope.modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'templates/yearCalendar/editEventYearTemplate.html',
+            controller: 'editEventYearController',
+            controllerAs: 'evYearCtrl',
+            bindToController: true,
+            resolve: {
+                rooms: function () {
+                    return $scope.availableRooms;
+                },
+                devices: function () {
+                    return $scope.availableInventory;
+                },
+                users: function () {
+                    return $scope.users;
+                },
+                selectedDate: function () {
+                    return $scope.selectedDate;
+                },
+                eventTypes: function () {
+                    return $scope.eventTypes;
+                },
+            }
+        });
+    };
+
+
+    $scope.selectedDate = $scope.selectedDate || currentDate;
+    $scope.eventSelected = false;
+    $scope.modalShown = false;
+    $scope.sidebarStyle = true;
+
+  
+    getRooms();
+    getInventory();
+    getUsers();
+    getAllEvents();
+    getEventTypes();
+
+
+    function getRooms() {
+        yearEventService.getAllRooms()
+            .$promise.then(
+                function(response) {
+                    console.log('success Total rooms: ', response.length);
+                    $scope.availableRooms = response;
+                },
+                function(response) {
+                    console.log('failure', response);
+                }
+            );
+    }
+
+    function getInventory() {
+        yearEventService.getAllDevices()
+            .$promise.then(
+                function(response) {
+                    console.log('success Inventory items: ', response.length);
+                    $scope.availableInventory = response;
+                },
+                function(response) {
+                    console.log('failure', response);
+                }
+            );
+    }
+
+    function getUsers() {
+        yearEventService.getAllUsers()
+            .$promise.then(
+                function(response) {
+                    console.log('success Number of Users: ', response.length);
+                    $scope.users = response;
+                },
+                function(response) {
+                    console.log('failure', response);
+                }
+            );
+    }
+
+    function getEventTypes() {
+        yearEventService.getAllEventTypes()
+            .$promise.then(
+                function(response) {
+                    console.log('success Current number of types: ', response.length);
+                    $scope.eventTypes = response;
+                },
+                function(response) {
+                    console.log('failure', response);
+                }
+            );
+    }
+
+    function getAllEvents() {
+        yearEventService.getAllEvents()
+            .$promise.then(
+                function(response) {
+                    console.log('success Number of Events: ', response.length);
+                    $scope.allEvents = response;
+                },
+                function(response) {
+                    console.log('failure', response);
+                }
+            );
     }
 
 }
