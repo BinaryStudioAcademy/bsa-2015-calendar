43fd3381284a235285957b134cf205c791d678ab
ag <andrey.grisch@gmail.com>
2015-11-05 22:35:03 +0200

    change get date by modal

--- frontend/javascripts/yearCalendar/calendarDirective.js
+++ frontend/javascripts/yearCalendar/calendarDirective.js
@@ -2,15 +2,14 @@ var app = require('../app');
 
 app.directive('calendarDirective', calendarDirective);
 
-calendarDirective.$inject = ['helpEventService'];
-
-function calendarDirective($animate, $timeout, $uibModal) {
+function calendarDirective($animate) {
     return {
         restrict: 'A',
         templateUrl: 'templates/yearCalendar/monthTemplate.html',
         scope: {
             calendar: '=',
-            monthNum: '@'
+            monthNum: '@',
+            modal: '&'
         },
 
         link: function ($scope, element, attr) {
@@ -41,94 +40,12 @@ function calendarDirective($animate, $timeout, $uibModal) {
                 $animate.enter(element.children('.year-month-table'), element); 
             }, true);   
         },
-
-        controller: function($scope, helpEventService) {
-            console.log($scope);
-
-            $scope.availableRooms = getRooms();
-            $scope.availableInventory = getInventory();
-            $scope.users = getUsers();
-            $scope.eventTypes = getEventTypes();
-
-
-            $scope.showCloseModal = function(month, dayDate) {
-                $scope.selectedDate = new Date($scope.calendar.year, month, dayDate);
-                console.log('selectedDate');
-                $scope.modalInstance = $uibModal.open({
-                    animation: true,
-                    templateUrl: 'templates/yearCalendar/editEventYearTemplate.html',
-                    controller: 'editEventYearController',
-                    controllerAs: 'evYearCtrl',
-                    bindToController: true,
-                    resolve: {
-                        rooms: function () {
-                            return $scope.availableRooms;
-                        },
-                        devices: function () {
-                            return $scope.availableInventory;
-                        },
-                        users: function () {
-                            return $scope.users;
-                        },
-                        eventTypes: function () {
-                            return $scope.eventTypes;
-                        },
-                    }
-                });
+        controller: function($scope) {
+            $scope.openModal = function($event) {
+                var clickElem = $event.target.attributes.id.value;
+                $scope.modal({clickElem: clickElem});
             };
-
-            function getRooms() {
-                helpEventService.getAllRooms()
-                    .$promise.then(
-                        function(response) {
-                            console.log('success Total rooms: ', response.length);
-                            $scope.availableRooms = response;
-                        },
-                        function(response) {
-                            console.log('failure', response);
-                        }
-                    );
-            }
-
-            function getInventory() {
-                helpEventService.getAllDevices()
-                    .$promise.then(
-                        function(response) {
-                            console.log('success Inventory items: ', response.length);
-                            $scope.availableInventory = response;
-                        },
-                        function(response) {
-                            console.log('failure', response);
-                        }
-                    );
-            }
-
-            function getUsers() {
-                helpEventService.getAllUsers()
-                    .$promise.then(
-                        function(response) {
-                            console.log('success Number of Users: ', response.length);
-                            $scope.users = response;
-                        },
-                        function(response) {
-                            console.log('failure', response);
-                        }
-                    );
-            }
-
-            function getEventTypes() {
-                helpEventService.getAllEventTypes()
-                    .$promise.then(
-                        function(response) {
-                            console.log('success Current number of types: ', response.length);
-                            $scope.eventTypes = response;
-                        },
-                        function(response) {
-                            console.log('failure', response);
-                        }
-                    );
-            }
-
         }
+ 
     };
 }
--- frontend/javascripts/yearCalendar/yearCalendarController.js
+++ frontend/javascripts/yearCalendar/yearCalendarController.js
@@ -2,9 +2,9 @@ var app = require('../app');
 
 app.controller('yearCalendarController', yearCalendarController);
 
-yearCalendarController.$inject = ['calendarService', '$scope'];
+yearCalendarController.$inject = ['calendarService', '$scope', '$uibModal', 'helpEventService'];
 
-function yearCalendarController(calendarService, $scope) {
+function yearCalendarController(calendarService, $scope, $uibModal, helpEventService) {
     var vm = this;
 
     //init with current year by default
@@ -40,7 +40,94 @@ function yearCalendarController(calendarService, $scope) {
         vm.currentYear = year || currentDate.getFullYear();
         vm.calendar = calendarService.getYearObj(vm.currentYear);
         getEvents(vm.currentYear);
+        vm.availableRooms = getRooms();
+        vm.availableInventory = getInventory();
+        vm.users = getUsers();
+        vm.eventTypes = getEventTypes();
+
     }
 
+    vm.showCloseModal = function(elemId) {
+        var elemDate = elemId.split('_');
+        vm.selectedDate = new Date(+elemDate[2], (+elemDate[1])-1, +elemDate[0]);
+        console.log(vm.selectedDate);
+        console.log(elemDate);
+        vm.modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'templates/yearCalendar/editEventYearTemplate.html',
+            controller: 'editEventYearController',
+            controllerAs: 'evYearCtrl',
+            bindToController: true,
+            resolve: {
+                rooms: function () {
+                    return vm.availableRooms;
+                },
+                devices: function () {
+                    return vm.availableInventory;
+                },
+                users: function () {
+                    return vm.users;
+                },
+                selectedDate: function () {
+                    return vm.selectedDate;
+                },
+                eventTypes: function () {
+                    return vm.eventTypes;
+                },
+            }
+        });
+    };
+
+    function getRooms() {
+        helpEventService.getRooms()
+            .then(
+                function(response) {
+                    console.log('success Total rooms: ', response.length);
+                    vm.availableRooms = response;
+                },
+                function(response) {
+                    console.log('failure', response);
+                }
+            );
+    }
+
+    function getInventory() {
+        helpEventService.getDevices()
+            .then(
+                function(response) {
+                    console.log('success Inventory items: ', response.length);
+                    vm.availableInventory = response;
+                },
+                function(response) {
+                    console.log('failure', response);
+                }
+            );
+    }
+
+    function getUsers() {
+        helpEventService.getUsers()
+            .then(
+                function(response) {
+                    console.log('success Number of Users: ', response.length);
+                    vm.users = response;
+                },
+                function(response) {
+                    console.log('failure', response);
+                }
+            );
+    }
+
+    function getEventTypes() {
+        helpEventService.getEventTypes()
+            .then(
+                function(response) {
+                    console.log('success Current number of types: ', response.length);
+                    vm.eventTypes = response;
+                },
+                function(response) {
+                    console.log('failure', response);
+                }
+            );
+    }
 
 }
